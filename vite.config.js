5e0b9d231b0bea662dab81e02496c995cbf2e15f
bnhat03 <130288216+bnhat03@users.noreply.github.com>
2025-07-09 10:21:07 +0700

    change BE url

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
   plugins: [react()],
   define: {
     // 'import.meta.env.VITE_BACKEND_URL': JSON.stringify('https://food-app-gvbhgyfabjcthbhd.southeastasia-01.azurewebsites.net'),
-    "import.meta.env.VITE_BACKEND_URL": JSON.stringify("http://localhost:8080"),
+    "import.meta.env.VITE_BACKEND_URL": JSON.stringify(
+      "https://be-pbl6.onrender.com"
+    ),
     "import.meta.env.VITE_AI_URL": JSON.stringify(
       "https://b238-14-191-112-76.ngrok-free.app"
     ),
